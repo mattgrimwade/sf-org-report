9535b4828639bba90fee8e48d88f60f512dab1ad
Matt Grimwade <mattgrimwade@brightgen.com>
2020-05-05 19:54:38 +0100

    code review

--- components/MetadataTable.js
+++ components/MetadataTable.js
@@ -9,37 +9,32 @@ function MetadataTable({columns, rows, selector, setParentSelectedRows, previous
     const selectAllCheckboxRef = React.useRef(null);
 
     const updateSelected = (rowKey) => {
-        console.log('rowKey ', rowKey);
         const row = rows[rowKey]
         const subjectId = row.id;
     
-        if (selectedIds[subjectId]) {
-            delete selectedIds[subjectId];
-        } else {
+        if (selectedIds[subjectId]) 
+            delete selectedIds[subjectId]; 
+        else 
             selectedIds[subjectId] = true;
-        }
-        const currentSelected = row.selected != null ? row.selected : row.defaultSelected;
+        
+        const currentSelected = row.selected == null ? row.defaultSelected : row.selected;
         rows[rowKey].selected = !currentSelected;
         
-        if (allSelected === true && indeterminateSelectAll === false) {
-            setIndeterminateSelectAll(true);
-        }
+        if (allSelected && indeterminateSelectAll === false) setIndeterminateSelectAll(true);
+
         setSelectedIds(Object.assign({}, selectedIds));
         setParentSelectedRows(selectedIds);    
     }
 
     const selectAll = () => {
-        if (allSelected === false) {
-            rows.forEach(row => {
-                row.selected = true;
+        rows.forEach(row => {
+            row.selected = !allSelected; 
+            if (allSelected) 
+                deleteSelectedIds[row.id]; 
+            else 
                 selectedIds[row.id] = true;
-            })
-        } else {
-            rows.forEach(row => {
-                row.selected = false;
-                delete selectedIds[row.id];
-            })
-        }
+        });
+
         setIndeterminateSelectAll(false);
         setSelectedIds(selectedIds);
         setParentSelectedRows(selectedIds);    
@@ -58,34 +53,36 @@ function MetadataTable({columns, rows, selector, setParentSelectedRows, previous
         <table className={`${styles.parent} mt-5 w-100`}>
             <thead>
                 <tr>
-                    {showSelector && <th><input ref={selectAllCheckboxRef} 
+                    {showSelector && (
+                        <th><input ref={selectAllCheckboxRef} 
                                                 onChange={selectAll} 
                                                 type="checkbox" 
                                                 value={true} 
                                                 id={`checkBoxAll_${type}`} 
                                                 className="mx-auto"/>
-                                            <label for={`checkBoxAll_${type}`} /></th>}
-
+                                            <label for={`checkBoxAll_${type}`} />
+                        </th>
+                    )}
                     {columns.map(header => <th key={header}>{header}</th>)}
                 </tr>
             </thead>
             <tbody>
                 {rows.map((row, index) => 
                     <tr key={index} className="py-3">     
-                        {showSelector && <td><input onChange={() => updateSelected(index)} 
+                        {showSelector && (
+                            <td><input onChange={() => updateSelected(index)} 
                                                     checked={row.selected == null ? row.defaultSelected : row.selected}
                                                     value={row.defaultSelected} 
                                                     type="checkbox" 
                                                     id={`checkBox_${index}_${type}`}/>
-                                            <label for={`checkBox_${index}_${type}`} />
-                                         </td>}
-                        {cells.map(cellData => <td>{cellData.value}</td>)}
+                                <label for={`checkBox_${index}_${type}`} />
+                            </td>
+                        )}
+                        {cells.map(cellData => (
+                            <td>{cellData.value}</td>
+                        ))}
                     </tr>
-                )}
-                
-                
-                <DataRow key={index} type={type} rowIndex={index} 
-                                                    updateSelection={updateSelected} showSelector={showSelector} {...row}/>)}
+                )}                
             </tbody>
         </table>
     );
